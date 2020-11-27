81dcbf8eecef68427cc01204c538773e2fe97c35
Jeff DeCola (TK1-PC GIT FOR WINDOWS) <jeff@keeperlabs.com>
2020-11-26 18:37:52 -0800

    updating video contianer

--- video_container/video_container.php
+++ video_container/video_container.php
@@ -4,50 +4,71 @@
 <script type="text/javascript" src="video_container/jqs/custom.js"></script>
 <script type="text/javascript" src="video_container/jqs/fitvids.js"></script>
 
-<div id="foo_video_container" class="video_container">
+<div id="video_container_wrapper">
 
-    <div class="video_items_container">
+    <!-- --------------------------------------------------------------------------------------------------------------------------- -->
+    <div class="example_header_container">
 
-        <!-- ITEM **************************************************************** -->
-        <div class="foo_video_item video_item_middle">
+        <h3> VIMEO EXAMPLE </h3>
+        
+    </div>
+    <!-- --------------------------------------------------------------------------------------------------------------------------- -->
+    
+    <div id="vimeo_video_container" class="video_container">
+    
+        <div class="video_items_container">
 
-            <!-- THIS DEFINES THE HEIGHT -->
-            <div id="foo_video_player">
-                <iframe src="//player.vimeo.com/video/86659716?title=0&amp;byline=0&amp;portrait=0"
-                    width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
-                </iframe>
+            <!-- ITEM **************************************************************** -->
+            <div class="vimeo_video_item video_item_middle">
+
+                <!-- THIS DEFINES THE HEIGHT -->
+                <div id="vimeo_video_player">
+                    <iframe src="//player.vimeo.com/video/86659716?title=0&amp;byline=0&amp;portrait=0"
+                         width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                    </iframe>
+                </div>
+
+                <!-- BACKGROUND - THIS WILL SCALE -->
+                <div id="vimeo_video_background">
+                </div>
+
             </div>
 
-            <!-- BACKGROUND - THIS WILL SCALE -->
-            <div id="foo_video_background">
+       </div>
+
+    </div>
+    <!-- END OF vimeo_video_container -->
+
+    <!-- --------------------------------------------------------------------------------------------------------------------------- -->
+    <div class="example_header_container">
+
+        <h3> YOUTUBE EXAMPLE </h3>
+
+    </div>
+    <!-- --------------------------------------------------------------------------------------------------------------------------- -->
+    
+    <div id="youtube_video_container" class="video_container">
+
+        <div class="video_items_container">
+
+            <!-- ITEM **************************************************************** -->
+            <div class="youtube_video_item video_item_middle">
+
+                <!-- THIS DEFINES THE HEIGHT -->
+                <div id="youtube_video_player">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/R-4X7ZALqPo" frameborder="0" allowfullscreen></iframe>
+                </div>
+
+                <!-- BACKGROUND - THIS WILL SCALE -->
+                <div id="youtube_video_background">
+                </div>
+
             </div>
 
         </div>
 
     </div>
-
+    <!-- END OF youtube_video_container -->
+    
 </div>
-<!-- END OF foo_video_container -->
-
-<div id="bar_video_container" class="video_container">
-
-    <div class="video_items_container">
-
-        <!-- ITEM **************************************************************** -->
-        <div class="bar_video_item video_item_middle">
-
-            <!-- THIS DEFINES THE HEIGHT -->
-            <div id="bar_video_player">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/R-4X7ZALqPo" frameborder="0" allowfullscreen></iframe>
-            </div>
-
-            <!-- BACKGROUND - THIS WILL SCALE -->
-            <div id="bar_video_background">
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
-<!-- END OF bar_video_container -->
+<!-- END OF video_container_wrapper -->
